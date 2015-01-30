4db6d1bf48c2eebceed627f301f7c23803b9a1ad
marysees <marisees@hotmail.com>
2015-01-30 20:12:02 +0100

    Inicial

--- app/src/main/java/com/example/tarde/terremotos/MainActivity.java
+++ app/src/main/java/com/example/tarde/terremotos/MainActivity.java
@@ -1,23 +1,75 @@
 package com.example.tarde.terremotos;
-/*pantalla inicial: fecha, filtror por intensidad del terremoto, boton buscar
+/*pantalla inicial: fecha, filtros por intensidad del terremoto, boton buscar
 * */
 
 
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.Spinner;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends ActionBarActivity {
+    private CalendarView calendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gestionSpinerEscala();
+        gestionarBuscar();
+        gestionarFecha();
+        
+
     }
 
+    
+    public void gestionSpinerEscala(){
+        Spinner spinnerEscala = (Spinner) findViewById(R.id.spEscala);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Escala, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerEscala.setAdapter(adapter);
+
+        }
+
+        public void onItemSelected( ) {
+
+    }
+    
+    public void gestionarBuscar(){
+
+        View boton = findViewById(R.id.btBuscar);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                    //indicamos que quiero que haga el evento
+                    Intent intent = new Intent();
+
+             }
+        });
+
+    }
+
+    public void gestionarFecha(){
+       calendar.getDate();
+    }
+    
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
